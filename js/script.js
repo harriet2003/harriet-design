aade4ab54aae2c715451167a8495821f988758d8
Harriet Eglinton <hattieeglinton@gmail.com>
2024-05-16 11:06:04 +1200

    Updated Layout
    I changed the layout of the prototype so applied these to the code
    
    Basic HTML structure

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*--Project Redirect Buttons--*/
+const budRedirect = document.querySelector('.budProjectDiv');
+
+budRedirect.addEventListener('click', function() {
+  window.location.href = './project-bud.html';
+});
+
+const smallDataRedirect = document.querySelector('.smallDataProjectDiv');
+
+smallDataRedirect.addEventListener('click', function() {
+  window.location.href = './project-smallData.html';
+});
+
+const btbRedirect = document.querySelector('.btbProjectDiv');
+
+btbRedirect.addEventListener('click', function() {
+  window.location.href = './project-beatTheBinge.html';
+});
