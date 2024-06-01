7fe8825ae2f2be29c0e5452fe27656af1af86e79
Harriet Eglinton <hattieeglinton@gmail.com>
2024-06-02 10:59:54 +1200

    Mobile curtain menu
    - Created overlay
    - Linked all the pages
    - Created html for desktop navbar (need to style)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+//Navbar curtain menu
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+const logoRedirect = document.querySelector('.logo');
+
+logoRedirect.addEventListener('click', function() {
+  window.location.href = './index.html';
+});
+
 /*--Project Redirect Buttons--*/
 const budRedirect = document.querySelector('.budProjectDiv');
 
