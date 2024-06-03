89b78186f8883ecbab78faca3bc1df4857f07e7c
Harriet Eglinton <hattieeglinton@gmail.com>
2024-06-03 20:41:54 +1200

    Major Progress - Desktop
    - Fixed the dektop version of the home page (v1)
    - Desktop navbar
    - Refined typography

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
+const mobileNavLogo = document.querySelector(".navLogo");
+
+mobileNavLogo.addEventListener("click", function () {
+   window.location.href = "./index.html";
+});
+
+const desktopNavLogo = document.querySelector(".desktopNavLogo");
+
+desktopNavLogo.addEventListener("click", function () {
+   window.location.href = "./index.html";
+});
+
 //Navbar curtain menu
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+   document.getElementById("myNav").style.width = "100%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+   document.getElementById("myNav").style.width = "0%";
 }
 
-const logoRedirect = document.querySelector('.logo');
-
-logoRedirect.addEventListener('click', function() {
-  window.location.href = './index.html';
-});
-
 /*--Project Redirect Buttons--*/
-const budRedirect = document.querySelector('.budProjectDiv');
+const budRedirect = document.querySelector(".budProjectDiv");
 
-budRedirect.addEventListener('click', function() {
-  window.location.href = './project-bud.html';
+budRedirect.addEventListener("click", function () {
+   window.location.href = "./project-bud.html";
 });
 
-const smallDataRedirect = document.querySelector('.smallDataProjectDiv');
+const smallDataRedirect = document.querySelector(".smallDataProjectDiv");
 
-smallDataRedirect.addEventListener('click', function() {
-  window.location.href = './project-smallData.html';
+smallDataRedirect.addEventListener("click", function () {
+   window.location.href = "./project-smallData.html";
 });
 
-const btbRedirect = document.querySelector('.btbProjectDiv');
+const btbRedirect = document.querySelector(".btbProjectDiv");
 
-btbRedirect.addEventListener('click', function() {
-  window.location.href = './project-beatTheBinge.html';
+btbRedirect.addEventListener("click", function () {
+   window.location.href = "./project-beatTheBinge.html";
 });
