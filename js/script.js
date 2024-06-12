13035de808d62f9cc01c04f9d3e7cce44c2fe99f
Harriet Eglinton <hattieeglinton@gmail.com>
2024-06-12 22:28:54 +1200

    Home Page Scroll Effect
    - blur scroll with hidden class

--- js/script.js
+++ js/script.js
@@ -40,6 +40,33 @@ btbRedirect.addEventListener("click", function () {
    window.location.href = "./project-beatTheBinge.html";
 });
 
+/* Scroll Effect */
+const observer = new IntersectionObserver((entries) => {
+   entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+         entry.target.classList.add("show");
+      }
+   });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+/* Scroll Effect */
+const observer2 = new IntersectionObserver((entries) => {
+   entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+         entry.target.classList.add("show2");
+      }
+   });
+});
+
+const hiddenElements2 = document.querySelectorAll(".hidden2");
+hiddenElements2.forEach((el) => observer2.observe(el));
+
 /* Facts Toggle Button */
 function toggleFacts() {
    var x = document.getElementById("factsPopup");
