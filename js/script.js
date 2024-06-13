8ca44ec415ad523915887a34965e468da9440969
Harriet Eglinton <hattieeglinton@gmail.com>
2024-06-13 21:27:01 +1200

    Scroll Effect v1
    - reveal on scroll on all pages applied to all section

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ function closeNav() {
    document.getElementById("myNav").style.width = "0%";
 }
 
+
 /*--Project Redirect Buttons--*/
 const budRedirect = document.querySelector(".budProjectCard");
 
@@ -40,32 +41,8 @@ btbRedirect.addEventListener("click", function () {
    window.location.href = "./project-beatTheBinge.html";
 });
 
-/* Scroll Effect */
-const observer = new IntersectionObserver((entries) => {
-   entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.isIntersecting) {
-         entry.target.classList.add("show");
-      }
-   });
-});
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
 
 
-/* Scroll Effect */
-const observer2 = new IntersectionObserver((entries) => {
-   entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.isIntersecting) {
-         entry.target.classList.add("show2");
-      }
-   });
-});
-
-const hiddenElements2 = document.querySelectorAll(".hidden2");
-hiddenElements2.forEach((el) => observer2.observe(el));
 
 /* Facts Toggle Button */
 function toggleFacts() {
