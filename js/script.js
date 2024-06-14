a88370c8ac077858cb7de0b56af3b33af573f542
Harriet Eglinton <hattieeglinton@gmail.com>
2024-06-14 14:37:43 +1200

    Flower Value Popups

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ function closeNav() {
    document.getElementById("myNav").style.width = "0%";
 }
 
-
 /*--Project Redirect Buttons--*/
 const budRedirect = document.querySelector(".budProjectCard");
 
@@ -41,8 +40,33 @@ btbRedirect.addEventListener("click", function () {
    window.location.href = "./project-beatTheBinge.html";
 });
 
+/* Value Flower Hover Effect */
+const hoverEmpathyText = document.getElementById("empathyText");
+const hoverEmpathyImg = document.getElementById("empathyFlower");
+const hoverConnectText = document.getElementById("connectText");
+const hoverConnectImg = document.getElementById("connectFlower");
+const hoverPlayText = document.getElementById("playText");
+const hoverPlayImg = document.getElementById("playFlower");
 
+hoverEmpathyText.addEventListener("mouseenter", function () {
+   hoverEmpathyImg.style.display = "unset";
+});
+hoverConnectText.addEventListener("mouseenter", function () {
+   hoverConnectImg.style.display = "unset";
+});
+hoverPlayText.addEventListener("mouseenter", function () {
+   hoverPlayImg.style.display = "unset";
+});
 
+hoverEmpathyText.addEventListener("mouseleave", function () {
+   hoverEmpathyImg.style.display = "none";
+});
+hoverConnectText.addEventListener("mouseleave", function () {
+   hoverConnectImg.style.display = "none";
+});
+hoverPlayText.addEventListener("mouseleave", function () {
+   hoverPlayImg.style.display = "none";
+});
 
 /* Facts Toggle Button */
 function toggleFacts() {
--- js/scroll.js
+++ js/scroll.js
@@ -1,26 +1,25 @@
 /* Scroll Effect */
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-       console.log(entry);
-       if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-       }
-    });
- });
- 
- const hiddenElements = document.querySelectorAll(".hidden");
- hiddenElements.forEach((el) => observer.observe(el));
- 
- 
- /* Scroll Effect */
- const observer2 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-       console.log(entry);
-       if (entry.isIntersecting) {
-          entry.target.classList.add("show2");
-       }
-    });
- });
- 
- const hiddenElements2 = document.querySelectorAll(".hidden2");
- hiddenElements2.forEach((el) => observer2.observe(el));
+   entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+         entry.target.classList.add("show");
+      }
+   });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+/* Scroll Effect */
+const observer2 = new IntersectionObserver((entries) => {
+   entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting) {
+         entry.target.classList.add("show2");
+      }
+   });
+});
+
+const hiddenElements2 = document.querySelectorAll(".hidden2");
+hiddenElements2.forEach((el) => observer2.observe(el));
